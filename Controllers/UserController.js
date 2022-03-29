2b22cc94390f7d5f660533d19c03e73f4886f6f1
r151149 <45209359+r151149@users.noreply.github.com>
2022-03-29 22:33:51 +0530

    connection script added

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -59,12 +59,12 @@ async function AddUser(user, res)
 
             if(!isUserExist)
             { 
-                var accessToken = jwt.sign(user.email="unknown@"+Math.floor(Date.now())/1000+"@gmail.com", "config.SECRETE_KEY"); 
+                var accessToken = jwt.sign(user.email="unknown@"+Math.floor(Date.now()/1000)+"@gmail.com", "config.SECRETE_KEY"); 
 
                 user.token = accessToken; user.isActive = 1; user.isDeleted = 0; user.lastLogin = Date.now();
 
                 await UserService.createUser(user, res);
-                return res.status(201).send("Ok");
+                return res.status(200).send("Ok");
             }
             else
             {
