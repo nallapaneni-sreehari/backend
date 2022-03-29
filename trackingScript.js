707f41924d41288de3ec4b6ea9e059a89b3eea94
r151149 <45209359+r151149@users.noreply.github.com>
2022-03-29 22:25:07 +0530

    tracking script added

---
+++ trackingScript.js
@@ -0,0 +1,15 @@
+
+checkRequest();
+
+function checkRequest()
+{
+  fetch('https://backend-149.herokuapp.com/',{
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  }).then(res=>{
+
+  });
+}
